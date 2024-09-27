68e86f7d48e847d53f5ea9ccb20928eb0c0d20c6
rafaelsvieira <rafael.souzavieira@gmail.com>
2024-09-27 17:00:57 -0300

    feat: Add check env and credentials

--- stack_checker.py
+++ stack_checker.py
@@ -1,6 +1,7 @@
 import os
 import boto3
 import logging
+from botocore.exceptions import ClientError
 import argparse
 
 class CloudFormationStackChecker:
@@ -9,14 +10,49 @@ class CloudFormationStackChecker:
         self.debug = debug
 
         self.logger = logging.getLogger(__name__)
+        logging.getLogger('botocore').setLevel(logging.DEBUG if debug else logging.CRITICAL)
         logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
 
         self.logger.debug("Initializing CloudFormationStackChecker...")
+
+        if not self.check_aws_environment_variables():
+            raise Exception("Missing required environment variables.")
 
         self.cloudformation = boto3.client(
             'cloudformation',
             region_name=os.getenv('AWS_REGION')
         )
+
+        if not self.check_access():
+            raise Exception("Access to CloudFormation is denied or invalid credentials.")
+
+    def check_aws_environment_variables(self):
+        required_vars = [
+            'AWS_ACCESS_KEY_ID',
+            'AWS_SECRET_ACCESS_KEY',
+            'AWS_SESSION_TOKEN',
+            'AWS_REGION'
+        ]
+
+        missing_vars = [var for var in required_vars if not os.getenv(var)]
+
+        if missing_vars:
+            self.logger.error(
+                f"Missing required environment variables: {', '.join(missing_vars)}"
+            )
+            return False
+
+        self.logger.debug("All required environment variables are set.")
+        return True
+
+    def check_access(self):
+        try:
+            self.cloudformation.describe_stacks()
+            self.logger.debug("Access to CloudFormation is verified.")
+            return True
+        except ClientError as e:
+            self.logger.error(f"Access check failed: {str(e)}")
+            return False
 
     def run(self):
         pass
