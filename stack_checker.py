04a6177ec8ccba9d2b0da93c1201d8c34c9bb6ff
rafaelsvieira <rafael.souzavieira@gmail.com>
2024-09-27 16:46:28 -0300

    feat: Add logger

--- stack_checker.py
+++ stack_checker.py
@@ -1,24 +1,36 @@
 import os
 import boto3
+import logging
 import argparse
 
 class CloudFormationStackChecker:
-    def __init__(self, stack_name):
+    def __init__(self, stack_name, debug=False):
         self.stack_name = stack_name
+        self.debug = debug
+
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
+
+        self.logger.debug("Initializing CloudFormationStackChecker...")
 
         self.cloudformation = boto3.client(
             'cloudformation',
             region_name=os.getenv('AWS_REGION')
         )
 
+    def run(self):
+        pass
+
 def main():
     parser = argparse.ArgumentParser(description='AWS CloudFormation Stack Checker')
     parser.add_argument('stack_name', type=str, help='The name of the CloudFormation stack')
+    parser.add_argument('--debug', action='store_true', help='Enable debug logging')
 
     args = parser.parse_args()
 
     try:
-        checker = CloudFormationStackChecker(stack_name=args.stack_name)
+        checker = CloudFormationStackChecker(stack_name=args.stack_name, debug=args.debug)
+        checker.run()
     except Exception as e:
         print(f"Error: {str(e)}")
 
